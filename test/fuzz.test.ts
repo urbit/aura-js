1e30add3818e02a4600d7c3fd88dddab032b86f1
fang <git@fang.io>
2025-06-13 20:50:53 +0200

    tests: add fuzz tests
    We disable auras for which fuzzing doesn't make sense, or that are
    known-unsupported.

---
+++ test/fuzz.test.ts
@@ -0,0 +1,48 @@
+import parse, { aura } from "../src/parse";
+import render from "../src/render";
+
+const testCount = 500;
+
+const auras: aura[] = [
+  // 'c',  //TODO  known-broken parsing
+  'da',
+  // 'dr', //TODO  unsupported
+  // 'f',  //  limited legitimate values
+  // 'n',  //  limited legitimate values
+  'p',
+  'q',
+  'sb',
+  'sd',
+  'si',
+  'sv',
+  'sw',
+  'sx',
+  // 't',    //  stdlib also crashes on rendering arbitrary bytes
+  // 'ta',   //
+  // 'tas',  //
+  'ub',
+  'ud',
+  'ui',
+  'uv',
+  'uw',
+  'ux'
+]
+
+function fuzz(nom: string, arr: Uint8Array | Uint16Array | Uint32Array | BigUint64Array) {
+  crypto.getRandomValues(arr);
+  auras.forEach((a) => {
+    describe(nom + ' @' + a, () => {
+      it('round-trips losslessly', () => {
+        arr.forEach((n) => {
+          n = BigInt(n);
+          expect(parse(a, render(a, n))).toEqual(n);
+        });
+      });
+    });
+  });
+}
+
+fuzz('8-bit', new Uint8Array(testCount));
+fuzz('16-bit', new Uint16Array(testCount));
+fuzz('32-bit', new Uint32Array(testCount));
+fuzz('64-bit', new BigUint64Array(testCount));
