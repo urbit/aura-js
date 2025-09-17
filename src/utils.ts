fb88a4c585096edf954ab693b77cb400d0d981d7
fang <git@fang.io>
2025-09-17 13:35:28 +0200

    various: remove unused files and utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,3 @@
-export function chunkFromRight(str: string, size: number) {
-  const numChunks = Math.ceil(str.length / size);
-  const chunks = new Array<string>(numChunks);
-
-  for (let i = numChunks - 1, o = str.length; i >= 0; --i, o -= size) {
-    let start = o - size;
-    let len = size;
-    if (start < 0) {
-      start = 0;
-      len = o;
-    }
-    chunks[i] = str.substr(start, len);
-  }
-
-  return chunks;
-}
-
 export function chunk<T>(arr: T[], size: number): T[][] {
   let chunk: T[] = [];
   let newArray = [chunk];
@@ -25,20 +8,6 @@ export function chunk<T>(arr: T[], size: number): T[][] {
     } else {
       chunk = [arr[i]];
       newArray.push(chunk);
-    }
-  }
-
-  return newArray;
-}
-
-export function dropWhile<T>(arr: T[], pred: (x: T) => boolean): T[] {
-  const newArray = arr.slice();
-
-  for (const item of arr) {
-    if (pred(item)) {
-      newArray.shift();
-    } else {
-      return newArray;
     }
   }
 
